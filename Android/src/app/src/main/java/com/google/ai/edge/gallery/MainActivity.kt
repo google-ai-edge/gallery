2e565bd97fe8ccfa2b84e6f5f734555852c266a1
BlindDeveloper <vladiks.ivanovs@gmail.com>
2025-09-14 18:21:37 +0300

    Setup secure flag

--- Android/src/app/src/main/java/com/google/ai/edge/gallery/MainActivity.kt
+++ Android/src/app/src/main/java/com/google/ai/edge/gallery/MainActivity.kt
@@ -61,7 +61,10 @@ class MainActivity : ComponentActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-
+    window.setFlags(
+      WindowManager.LayoutParams.FLAG_SECURE,
+      WindowManager.LayoutParams.FLAG_SECURE
+    )
     modelManagerViewModel.loadModelAllowlist()
 
     // Show splash screen.
