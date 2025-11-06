5c954421d7537b7b8836893626de2450b212b6ae
TrialBlazer23 <trialblazer212@gmail.com>
2025-11-06 16:51:37 -0600

    Update build.gradle.kts to modify plugin aliases

--- Android/src/build.gradle.kts
+++ Android/src/build.gradle.kts
@@ -1,10 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose.compiler) apply false
-    alias(libs.plugins.hilt) apply false
-    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.hilt.application) apply false
     alias(libs.plugins.protobuf) apply false
+    alias(libs.plugins.oss.licenses) apply false
+    // Google Services removed - optional Firebase feature
 }
