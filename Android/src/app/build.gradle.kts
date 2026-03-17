8a3dd0547ffe69f9c1a572744e2a554e4dcf027c
Zichuan Wei <zichuanwei@google.com>
2026-03-17 12:01:22 -0700

    Increment version code to 1.0.11.
    PiperOrigin-RevId: 885139572

--- Android/src/app/build.gradle.kts
+++ Android/src/app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
     applicationId = "com.google.aiedge.gallery"
     minSdk = 31
     targetSdk = 35
-    versionCode = 19
-    versionName = "1.0.10"
+    versionCode = 20
+    versionName = "1.0.11"
 
     // Needed for HuggingFace auth workflows.
     // Use the scheme of the "Redirect URLs" in HuggingFace app.
