8ade5d1ded04e6cffcc9d5b9d93fc79a486447be
gzhou4895-alt <gzhou4895@gmail.com>
2026-04-27 19:05:11 +0800

    Integrate model manager with LiteRtApiServer
    Added model manager to API server initialization.

--- Android/src/app/src/main/java/com/google/ai/edge/gallery/MainActivity.kt
+++ Android/src/app/src/main/java/com/google/ai/edge/gallery/MainActivity.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.ai.edge.gallery
 
 import android.animation.ObjectAnimator
@@ -5,7 +21,6 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
-import android.net.wifi.WifiManager
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -150,6 +165,7 @@ class MainActivity : ComponentActivity() {
   private fun startApiServer() {
     try {
       apiServer = LiteRtApiServer(this, 8088)
+      apiServer?.setModelManager(modelManagerViewModel)
       apiServer?.startServer()
 
       var ipAddress = "N/A"
