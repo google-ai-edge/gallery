f7c8144d03ea14f05cafb18a43532bccf007018c
Zichuan Wei <zichuanwei@google.com>
2026-06-05 11:24:26 -0700

    Update AI Edge Gallery version to 1.0.16 (34).
    PiperOrigin-RevId: 927395302

--- Android/src/app/build.gradle.kts
+++ Android/src/app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
     applicationId = "com.google.aiedge.gallery"
     minSdk = 31
     targetSdk = 35
-    versionCode = 33
-    versionName = "1.0.15"
+    versionCode = 34
+    versionName = "1.0.16"
 
     // Needed for HuggingFace auth workflows.
     // Use the scheme of the "Redirect URLs" in HuggingFace app.
