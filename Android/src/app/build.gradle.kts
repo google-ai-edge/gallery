bd59ace64de4164f9bf9103828887c6693d91c5d
Zichuan Wei <zichuanwei@google.com>
2026-06-22 18:06:28 -0700

    Increment version code to 35.
    PiperOrigin-RevId: 936348374

--- Android/src/app/build.gradle.kts
+++ Android/src/app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
     applicationId = "com.google.aiedge.gallery"
     minSdk = 31
     targetSdk = 37
-    versionCode = 34
+    versionCode = 35
     versionName = "1.0.16"
 
     // Needed for HuggingFace auth workflows.
